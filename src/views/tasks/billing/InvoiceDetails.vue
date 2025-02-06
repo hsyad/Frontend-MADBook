6a3e3ddf097a7497fa2dba49e2fb272c9dc1b900
hsyad <78629701+hsyad@users.noreply.github.com>
2025-02-06 17:29:11 +0800

    Update documents details

---
+++ src/views/tasks/billing/InvoiceDetails.vue
@@ -0,0 +1,197 @@
+<template>
+    <main>
+        <div class="p-3 bg-white rounded-md">
+            <div class="d-flex mb-4 justify-between items-center">
+                <h2 class="fw-semibold mb-0">Invoice Details</h2>
+                <div class="d-flex gap-2">
+                    <button class="btn btn-primary rounded-md text-white font-bold h-12 w-20">
+                        <span class="material-icons text-white align-middle">
+                            download
+                        </span>
+                    </button>
+                    <button class="btn btn-success rounded-md text-white font-bold h-12 w-20">Edit</button>
+                </div>
+            </div>
+            <div class="border border-dark rounded p-4 ">
+                <!-- <div class="px-3" v-if="quotation"> -->
+                <!-- UI Testing -->
+                <div class="px-3">
+                    <!-- UI Testing -->
+                    <h2 class="mt-4 text-end fw-semibold">Invoice</h2>
+
+                    <!-- LOGO -->
+                    <div class="d-flex items-center my-4">
+                        <!-- <div v-if="quotation.logo" class="d-inline-block overflow-hidden h-20">
+                        <img :src="quotation.logo" alt="logo" style="height: 100%; object-fit: contain;">
+                    </div> -->
+
+                        <!-- UI Testing -->
+                        <div v-if="logo" class="d-inline-block overflow-hidden h-20">
+                            <img :src="logo" alt="logo" style="height: 100%; object-fit: contain;">
+                        </div>
+                    </div>
+
+                    <p class="text-secondary small">
+                        <!-- {{ quotation.business_address }} | {{ quotation.email_from }} -->
+                        {{ business_address }} | {{ email_from }} <!--UI Testing-->
+                    </p>
+
+                    <!-- CUSTOMER DETAILS -->
+                    <div class="d-flex justify-between my-4">
+                        <div class="text-start flex-grow-1">
+                            <p class="mb-1"><strong>Bill To:</strong></p>
+                            <!-- <p class="mb-1 fs-6"><strong>{{ quotation.c_name }}</strong></p> -->
+                            <p class="mb-1 fs-6">{{ c_name }}</p> <!--UI Testing-->
+                            <!-- <p class="mb-1 fs-6"><strong>{{ quotation.c_no }}</strong></p> -->
+                            <p class="mb-1 fs-6">{{ c_no }}</p> <!--UI Testing-->
+                            <!-- <p class="mb-1 fs-6 text-wrap"><strong>{{ quotation.c_address }}</strong></p> -->
+                            <p class="mb-1 fs-6 whitespace-pre">{{ c_address }}</p> <!--UI Testing-->
+                        </div>
+
+                        <!-- DO DETAILS -->
+                        <div class="container d-flex gap-4 justify-end">
+                            <div class="text-end">
+                                <!-- <p class="mb-1"><strong>Quote No: </strong>{{ quotation.id }}</p> -->
+                                <p class="mb-1"><strong>Invoice No: </strong></p> <!--UI Testing-->
+                                <!-- <p class="mb-1"><strong>Issue Date: </strong>{{ quotation.issue_date }}</p> -->
+                                <p class="mb-1"><strong>Issue Date: </strong></p> <!--UI Testing-->
+                                <!-- <p class="mb-1"><strong>Valid Until: </strong>{{ quotation.valid_date }}</p> -->
+                                <p class="mb-1"><strong>Delivery Date: </strong></p>
+                                <!--UI Testing-->
+                                <p class="mb-1"><strong>Due Date: </strong></p> <!--UI Testing-->
+                            </div>
+                            <div class="text-start">
+                                <p class="mb-1">{{ referenceNumber }}</p> <!--UI Testing-->
+                                <p class="mb-1">{{ issue_date }}</p> <!--UI Testing-->
+                                <p class="mb-1">{{ delivery_date }}</p>
+                                <p class="mb-1">{{ due_date }}</p> <!--UI Testing-->
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- TABLE -->
+                    <table class="table table-borderless shadow-none">
+                        <thead class="table-secondary">
+                            <tr>
+                                <th class="w-14">No.</th>
+                                <th class="w-auto text-start">Items</th>
+                                <th class="w-20">Price</th>
+                                <th class="w-25">Quantity</th>
+                                <th class="w-20">Amount</th>
+                            </tr>
+                        </thead>
+                        <tbody class="border-bottom">
+                            <tr v-for="(item, index) in items" :key="index">
+                                <td>{{ index + 1 }}</td>
+                                <td class="text-start">{{ item.name }}</td>
+                                <td>{{ item.price }}</td>
+                                <td>{{ item.quantity }}</td>
+                                <td>{{ item.amount }}</td>
+                            </tr>
+                        </tbody>
+                    </table>
+
+                    <div class="d-flex gap-3 justify-end mt-6">
+                        <div class="text-end">
+                            <p class="mb-1">Ship By: </p>
+                            <p>Shipping Fee: </p>
+                            <p class="fw-semibold">Total: </p>
+                        </div>
+                        <div class="text-end">
+                            <p class="mb-1">{{ ship_by }}</p>
+                            <p>RM{{ ship_fee }}</p>
+                            <p class="fw-semibold">RM{{ calculate_total }}</p>
+                        </div>
+                    </div>
+
+                    <!-- <p class="mt-3"><strong>Notes: </strong>{{ quotation.notes }}</p> -->
+                    <p class="mt-5"><strong>Notes: </strong>{{ notes }}</p> <!--UI Testing-->
+                    <div class="mt-4 mb-4 border-s-8 border-emerald-500 pl-4">
+                        <p class="mt-3 mb-px text-sm"> {{ bank_name }}</p>
+                        <p class="mb-px text-sm"> {{ acc_holder }}</p>
+                        <p class="mb-px text-sm"> {{ acc_num }}</p>
+                    </div>
+                </div>
+                <!-- <div v-else>
+                <p>Loading...</p>
+            </div> -->
+            </div>
+            <div class="flex m-2 justify-center">
+
+            </div>
+            <div class="d-flex gap-1 mb-2">
+                <button class="btn btn-success rounded-md text-white font-bold w-full">Create Delivery Order</button>
+                <button class="btn btn-success rounded-md text-white font-bold w-full">Create Invoice</button>
+            </div>
+        </div>
+    </main>
+</template>
+
+<script>
+import axios from 'axios';
+import Swal from 'sweetalert2';
+import { computed, ref } from 'vue';
+import logoImg from '/src/assets/images/kucen.png';
+
+export default {
+    setup() {
+        const logo = ref(logoImg);
+        //INFO
+        const business_address = ref("123 Business Street, City, State");
+        const email_from = ref("info@business.com");
+        //CUSTOMER
+        const c_name = ref("John Doe");
+        const c_no = ref("+60123456789");
+        const c_address = ref("Megan Ambassy, Taman U Thant, \n55000 Kuala Lumpur, \nWilayah Persekutuan Kuala Lumpur");
+        const referenceNumber = ref("#12345");
+        //DATES
+        const issue_date = ref("2025-01-28");
+        const delivery_date = ref("2025-02-28");
+        const due_date = ref("2025-02-28");
+        //ITEMS
+        const items = ref([
+            { name: "Item 1", price: 100.0, quantity: 2, amount: 200.0 },
+            { name: "Item 2", price: 50.0, quantity: 1, amount: 50.0 },
+        ]);
+        const calculate_total = ref("250.00");
+        const notes = ref("Please ensure payment within the validity period.");
+        //SHIPPING
+        const ship_by = ref("Ninjavan");
+        const ship_fee = ref("10.00");
+
+        //BANK DETAILS
+        const bank_name = ref("Maybank Malaysia");
+        const acc_holder = ref("Ali bin Abu");
+        const acc_num = ref("1234567890");
+
+        return {
+            logo,
+            business_address,
+            email_from,
+            c_name,
+            c_no,
+            c_address,
+            referenceNumber,
+            issue_date,
+            delivery_date,
+            due_date,
+            items,
+            notes,
+            calculate_total,
+            ship_by,
+            ship_fee,
+            bank_name,
+            acc_holder,
+            acc_num,
+        };
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+.material-icons {
+    font-size: 1.6rem; // icons size
+    color: var(--light);
+    transition: 0.2s ease-out;
+}
+</style>
--- src/views/tasks/billing/QuoteDetails.vue
+++ src/views/tasks/billing/QuoteDetails.vue
@@ -1,14 +1,212 @@
 <template>
     <main>
-        <div>
-            <h3>Quotation Details</h3>
-            <p><strong>Quote No: {{ quote.id }}</strong></p>
+        <div class="p-3 bg-white rounded-md">
+            <div class="d-flex mb-4 justify-between items-center">
+                <h2 class="fw-semibold mb-0">Quotation Details</h2>
+                <div class="d-flex gap-2">
+                    <button class="btn btn-primary rounded-md text-white font-bold h-12 w-20">
+                        <span class="material-icons text-white align-middle">
+                            download
+                        </span>
+                    </button>
+                    <button class="btn btn-success rounded-md text-white font-bold h-12 w-20">Edit</button>
+                </div>
+            </div>
+            <div class="border border-dark rounded p-4 max-h-[90vh]">
+                <!-- <div class="px-3" v-if="quotation"> -->
+                <!-- UI Testing -->
+                <div class="px-3">
+                    <!-- UI Testing -->
+                    <h2 class="mt-4 text-end fw-semibold">Quote</h2>
+
+                    <!-- LOGO -->
+                    <div class="d-flex items-center my-4">
+                        <!-- <div v-if="quotation.logo" class="d-inline-block overflow-hidden h-20">
+                        <img :src="quotation.logo" alt="logo" style="height: 100%; object-fit: contain;">
+                    </div> -->
+
+                        <!-- UI Testing -->
+                        <div v-if="logo" class="d-inline-block overflow-hidden h-20">
+                            <img :src="logo" alt="logo" style="height: 100%; object-fit: contain;">
+                        </div>
+                    </div>
+
+                    <p class="text-secondary small">
+                        <!-- {{ quotation.business_address }} | {{ quotation.email_from }} -->
+                        {{ business_address }} | {{ email_from }} <!--UI Testing-->
+                    </p>
+
+                    <!-- CUSTOMER DETAILS -->
+                    <div class="d-flex justify-between my-4">
+                        <div class="text-start flex-grow-1">
+                            <p class="mb-1"><strong>For:</strong></p>
+                            <!-- <p class="mb-1 fs-6"><strong>{{ quotation.c_name }}</strong></p> -->
+                            <p class="mb-1 fs-6">{{ c_name }}</p> <!--UI Testing-->
+                            <!-- <p class="mb-1 fs-6"><strong>{{ quotation.c_no }}</strong></p> -->
+                            <p class="mb-1 fs-6">{{ c_no }}</p> <!--UI Testing-->
+                            <!-- <p class="mb-1 fs-6 text-wrap"><strong>{{ quotation.c_address }}</strong></p> -->
+                            <p class="mb-1 fs-6 text-wrap" v-html="formattedAddress"></p> <!--UI Testing-->
+                        </div>
+
+                        <!-- QUOTATION DETAILS -->
+                        <div class="text-start">
+                            <!-- <p class="mb-1"><strong>Quote No: </strong>{{ quotation.id }}</p> -->
+                            <p class="mb-1"><strong>Quote No: </strong>{{ referenceNumber }}</p> <!--UI Testing-->
+                            <!-- <p class="mb-1"><strong>Issue Date: </strong>{{ quotation.issue_date }}</p> -->
+                            <p class="mb-1"><strong>Issue Date: </strong>{{ issue_date }}</p> <!--UI Testing-->
+                            <!-- <p class="mb-1"><strong>Valid Until: </strong>{{ quotation.valid_date }}</p> -->
+                            <p class="mb-1"><strong>Valid Until: </strong>{{ valid_date }}</p> <!--UI Testing-->
+                        </div>
+                    </div>
+
+                    <!-- ITEM DETAILS -->
+                    <table class="w-full border border-gray-300 text-center shadow-none">
+                        <thead class="bg-gray-200 border border-gray-300 text-black">
+                            <tr>
+                                <th scope="col" class="small">No.</th>
+                                <th scope="col" class="small">Items</th>
+                                <th scope="col" class="small">Price</th>
+                                <th scope="col" class="small">Quantity</th>
+                                <th scope="col" class="small">Total</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <!-- <tr v-for="(item, index) in quotation.items" :key="index"> -->
+                            <tr v-for="(item, index) in items" :key="index"> <!--UI Testing-->
+                                <td class="px-4 py-2 border">{{ index + 1 }}</td>
+                                <td class="px-4 py-2 border">{{ item.name }}</td>
+                                <td class="px-4 py-2 border text-end">RM{{ item.price }}</td>
+                                <td class="px-4 py-2 border text-end">{{ item.quantity }}</td>
+                                <td class="px-4 py-2 border text-end">RM{{ (item.price * item.quantity).toFixed(2) }}
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+
+                    <!-- <p class="mt-3"><strong>Notes: </strong>{{ quotation.notes }}</p> -->
+                    <p class="mt-5"><strong>Notes: </strong>{{ notes }}</p> <!--UI Testing-->
+                    <p class="text-end fs-5 fw-bold">Total: RM{{ calculate_total }}</p>
+                </div>
+                <!-- <div v-else>
+                <p>Loading...</p>
+            </div> -->
+            </div>
+            <div class="flex m-2 justify-center">
+
+            </div>
+            <div class="d-flex gap-1 mb-2">
+                <button class="btn btn-success rounded-md text-white font-bold w-full">Create Delivery Order</button>
+                <button class="btn btn-success rounded-md text-white font-bold w-full">Create Invoice</button>
+            </div>
         </div>
     </main>
 </template>
 
 <script>
-export default {
+import axios from 'axios';
+import Swal from 'sweetalert2';
+import { computed, ref } from 'vue';
+import logoImg from '/src/assets/images/kucen.png';
 
-}
+export default {
+    //     name: "QuoteDetails",
+    //     data() {
+    //         return {
+    //             // quotation: null,
+    //             logo: null, // Replace with actual logo URL or null
+    //             business_address: "123 Business Street, City, State",
+    //             email_from: "info@business.com",
+    //             c_name: "John Doe",
+    //             c_no: "+60123456789",
+    //             c_address: "Megan Ambassy, Taman U Thant, 55000 Kuala Lumpur, Wilayah Persekutuan Kuala Lumpur",
+    //             referenceNumber: "Q12345",
+    //             issue_date: "2025-01-28",
+    //             valid_date: "2025-02-28",
+    //             items: [
+    //                 { name: "Item 1", price: 100.0, quantity: 2 },
+    //                 { name: "Item 2", price: 50.0, quantity: 1 },
+    //             ],
+    //             notes: "Please ensure confirmation order within the validity period.",
+    //         };
+    //     },
+    //     // created() {
+    //     //     const id = this.$route.params.id; // Get the quote ID from the URL
+    //     //     this.fetchQuoteDetails(quoteId);
+    //     // },
+    //     // methods: {
+    //     //     async fetchQuoteDetails(quoteId) {
+    //     //         try {
+    //     //             const response = await axios.get(`http://localhost/api/quotation/${quoteId}`);
+    //     //             this.quotation = response.data; // Store the quotation data in the 'quotation' var
+    //     //         } catch (error) {
+    //     //             Swal.fire({
+    //     //                 icon: 'error',
+    //     //                 title: "Oops...",
+    //     //                 text: "An error occured while fetching quotation details. Please try again.",
+    //     //                 confirmButtonText: 'Try Again',
+    //     //             }).then((result) => {
+    //     //                 if (result.isConfirmed) {
+    //     //                     this.fetchQuotationDetails(quoteId); // Retry the request
+    //     //                 }
+    //     //             });
+    //     //         }
+    //     //     },
+    //     // },
+    //     computed: {
+    //         calculateTotal() {
+    //             // return this.quotation.items.reduce((total, item) => total + item.price * item.quantity, 0).toFixed(2);
+    //             return this.items.reduce((total, item) => total + item.price * item.quantity, 0).toFixed(2); //UI Testing
+
+    //         },
+    //     },
+
+    setup() {
+        const logo = ref(logoImg);
+        const business_address = ref("123 Business Street, City, State");
+        const email_from = ref("info@business.com");
+        const c_name = ref("John Doe");
+        const c_no = ref("+60123456789");
+        const c_address = ref("Megan Ambassy, Taman U Thant, \n55000 Kuala Lumpur, \nWilayah Persekutuan Kuala Lumpur");
+        const referenceNumber = ref("Q12345");
+        const issue_date = ref("2025-01-28");
+        const valid_date = ref("2025-02-28");
+        const items = ref([
+            { name: "Item 1", price: 100.0, quantity: 2 },
+            { name: "Item 2", price: 50.0, quantity: 1 },
+        ]);
+        const notes = ref("Please ensure confirmation order within the validity period.");
+
+        const calculate_total = computed(() => {
+            return items.value.reduce((total, item) => total + item.price * item.quantity, 0).toFixed(2);
+        });
+
+        const formattedAddress = computed(() => {
+            return c_address.value.replace(/\n/g, '<br>');
+        });
+
+        return {
+            logo,
+            business_address,
+            email_from,
+            c_name,
+            c_no,
+            c_address,
+            referenceNumber,
+            issue_date,
+            valid_date,
+            items,
+            notes,
+            calculate_total,
+            formattedAddress,
+        };
+    }
+};
 </script>
+
+<style lang="scss" scoped>
+.material-icons {
+    font-size: 1.6rem; // icons size
+    color: var(--light);
+    transition: 0.2s ease-out;
+}
+</style>
